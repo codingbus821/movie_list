2bf80a9b13709e30c8514bac0641a1cc6f2eafbe
kimbyeongsu <kbsu147@gmail.com>
2021-05-07 13:48:21 +0900

    first commit

---
+++ app/src/main/java/com/example/movielist/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.movielist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.ActionBar
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var actionBar : ActionBar?
+
+        actionBar = supportActionBar;
+        actionBar?.hide();
+    }
+}
